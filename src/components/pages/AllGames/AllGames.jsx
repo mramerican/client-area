b3b458a19ebe8deef6ca38967aee85c15f4186fb
mramerican <mramerican@i.ua>
2023-06-07 19:47:40 +0200

    fix style

--- src/components/pages/AllGames/AllGames.jsx
+++ src/components/pages/AllGames/AllGames.jsx
@@ -1,12 +1,18 @@
-import ViewType from 'components/pages/AllGames/ViewType/ViewType';
+import classNames from 'classnames';
 import React from 'react';
 
 import Content from 'components/pages/AllGames/Content/Content';
 import Sort from 'components/pages/AllGames/Sort/Sort';
+import ViewType from 'components/pages/AllGames/ViewType/ViewType';
+
+import { useSelector } from 'react-redux';
+import { allGamesSelector } from 'store/slices/allGames';
 
 import styles from './AllGames.module.scss';
 
 const AllGames = () => {
+  const viewType = useSelector(allGamesSelector.getViewType);
+
   return (
     <>
       <div className={styles.wrapperHeader}>
@@ -14,7 +20,11 @@ const AllGames = () => {
           <div className={styles.filters}>filters</div>
         </div>
         <div className={styles.wrapperSort}>
-          <div className={styles.contentSort}>
+          <div
+            className={classNames(styles.contentSort, {
+              [styles.columnType]: viewType === 'column',
+            })}
+          >
             <Sort />
             <ViewType />
           </div>
